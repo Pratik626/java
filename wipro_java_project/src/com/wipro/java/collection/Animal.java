5198e7982345893ef9f792dd5d30634fca585fc5
Pratik <shubhsingh1311@gmail.com>
2025-02-06 12:59:12 +0530

    day5

--- wipro_java_project/src/com/wipro/java/collection/Animal.java
+++ wipro_java_project/src/com/wipro/java/collection/Animal.java
@@ -1,69 +1,59 @@
-package com.wipro.java.collection;
+package com.wipro.java.collection;  // This defines the package for the class
 
-import java.util.*;
+import java.util.*;  // This imports the necessary classes for using lists and sorting
 
-class Animal implements Comparable<Animal> {
-    private String name; // Name of the animal
-    private int age;     // Age of the animal
+public class Animal {  // This is the Animal class that represents an animal object
+    private String name; // This stores the name of the animal
+    private int age;     // This stores the age of the animal
 
-    // Constructor
+    // This is the constructor to create an Animal object with a name and age
     public Animal(String name, int age) {
-        this.name = name;
-        this.age = age;
+        this.name = name;  // Set the animal's name
+        this.age = age;    // Set the animal's age
     }
 
-    //Compare animals based on age (Using Comparable)
-    public int compareTo(Animal a) {
-        return Integer.compare(this.age, a.age); // Compare ages
-    }
-
-    //using Getters get the name 
+    // This is a method that returns the name of the animal
     public String getName() {
-        return name;
-    }
-    
-    //using Getters get the age
-    public int getAge() {
-        return age;
+        return name;  // Return the name
     }
 
-    // Override toString for easy display
+    // This is a method that returns the age of the animal
+    public int getAge() {
+        return age;   // Return the age
+    }
+
+    // This method is used to print the animal's details (name and age) in a readable format
     @Override
     public String toString() {
-        return name + " " + age;
+        return name + " " + age;  // Combine the name and age into a single string
     }
 
-    // Using Comparator to compare animals based on name (Alphabetical Order)
-    public static class AnimalNameComparator implements Comparator<Animal> {
-        @Override
-        public int compare(Animal a1, Animal a2) {
-            return a1.getName().compareTo(a2.getName()); // Compare by name alphabetically
-        }
-    }
-
+    // Main method where the program starts
     public static void main(String[] args) {
-        ArrayList<Animal> animals = new ArrayList<>();
-        animals.add(new Animal("Lion", 5));
-        animals.add(new Animal("Elephant", 10));
-        animals.add(new Animal("Zebra", 5));
-        animals.add(new Animal("Giraffe", 7));
+        ArrayList<Animal> animals = new ArrayList<>();  // Create a list to store Animal objects
 
-        // Sort the list based on age using Comparable
-        Collections.sort(animals);
+        // Add animals to the list with names and ages
+        animals.add(new Animal("Lion", 5));  // Add a Lion that is 5 years old
+        animals.add(new Animal("Elephant", 10));  // Add an Elephant that is 10 years old
+        animals.add(new Animal("Zebra", 6));  // Add a Zebra that is 5 years old
+        animals.add(new Animal("Giraffe", 7));  // Add a Giraffe that is 7 years old
 
-        // Print the sorted list by age
-        System.out.println("Animals after sorting by age by using Comparable:");
-        for (Animal animal : animals) {
-            System.out.println(animal);
+        // Sort the list of animals by age using the AnimalComparable class
+        Collections.sort(animals, new AnimalComparable());  // Sorting animals by age
+
+        // Print out the list of animals sorted by age
+        System.out.println("Animals after sorting by age Using Comparable:");
+        for (Animal animal : animals) {  // Go through each animal in the sorted list
+            System.out.println(animal);  // Print the animal's name and age
         }
 
-        // Sort the list based on name using the AnimalNameComparator (Comparator)
-        Collections.sort(animals, new AnimalNameComparator());
+        // Sort the list of animals by name using the AnimalComparator class
+        Collections.sort(animals, new AnimalComparator());  // Sorting animals by name
 
-        // Print the sorted list by name
-        System.out.println("\nAnimals after sorting by name by using Comparator:");
-        for (Animal animal : animals) {
-            System.out.println(animal);
+        // Print out the list of animals sorted by name
+        System.out.println("\nAnimals after sorting by name using Comparator:");
+        for (Animal animal : animals) {  // Go through each animal in the sorted list
+            System.out.println(animal);  // Print the animal's name and age
         }
     }
 }
