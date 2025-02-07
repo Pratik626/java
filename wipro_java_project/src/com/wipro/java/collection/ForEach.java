174990c5b36362aa1372288ead1ca0c53363e775
Pratik <shubhsingh1311@gmail.com>
2025-02-07 11:47:05 +0530

    day5

---
+++ wipro_java_project/src/com/wipro/java/collection/ForEach.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.wipro.java.collection;
+
+/**
+ * 
+ */
+public class ForEach {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int arr[] = { 10, 20, 30, 40, 50 };
+		// Using for-each loop to
+		// introduced in Java 5
+		//Initialization not required, start form 0 till end
+		//condition not required
+		//Implementation not required
+		for (int e : arr) {
+			System.out.println(e);
+		}
+	}
+
+}
